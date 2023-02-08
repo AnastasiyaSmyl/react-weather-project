132a6c697b57d01e4d06e02158f0947addac4bed
AnastasiyaSmyl <anastasiya8000@gmail.com>
2023-02-08 23:24:52 +0200

    Created component Weather/div.Weather

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+import ReactAnimatedWeather from "react-animated-weather";
 import "./Weather.css";
 
 export default function Weather() {
@@ -10,7 +11,30 @@ export default function Weather() {
         <br />
         Ukraine{" "}
       </h1>
-      <h5> Wednesday, 8 February </h5>
+      <ul>
+        <li> Wednesday, 8 February </li>
+        <li>Snowing </li>
+      </ul>
+
+      <div className="row">
+        <div className="col-6">
+          <ReactAnimatedWeather
+            icon="CLEAR_NIGHT"
+            color="grey"
+            size={48}
+            animate={true}
+          />{" "}
+          <h2>-8℃</h2>
+        </div>
+
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 0 mm</li>
+            <li>Humidity: 83%</li>
+            <li>Wind: 6 km/h</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
