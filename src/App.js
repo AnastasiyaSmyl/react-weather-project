706f31ad14fd166a47897095381f08133779a70a
AnastasiyaSmyl <anastasiya8000@gmail.com>
2023-02-08 22:25:57 +0200

    Added Weather component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import Weather from './Weather';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-    Hello
-    </div>
+        Start
+        <Weather />
+      </div>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <h1>
+        Kyiv,
+        <br />
+        Ukraine{" "}
+      </h1>
+      <h5> Wednesday, 8 February </h5>
+    </div>
+  );
+}
