3671923003377860a839621a2d34f73c8ab07f45
AnastasiyaSmyl <anastasiya8000@gmail.com>
2023-02-09 14:20:35 +0200

    Footer added links
    Added links GitHub and Netlify

--- src/Footer.js
+++ src/Footer.js
@@ -8,8 +8,22 @@ export default function Footer(){
         <div className="code-inform">
           <small>
             This project was coded <a href="/">Anastasiya Smyslova</a> and is{" "}
-            <a href="/">open-sourced</a> on GitHub and hosted on{" "}
-            <a href="/"> Netlify</a>
+            <a
+              href="https://github.com/AnastasiyaSmyl/react-weather-project"
+              target="_blank"
+              rel="noreferrer"
+            >
+              open-sourced on GitHub
+            </a>{" "}
+            and hosted on
+            <a
+              href="https://leafy-seahorse-bc54bb.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              Netlify
+            </a>
           </small>
         </div>
       </footer>
